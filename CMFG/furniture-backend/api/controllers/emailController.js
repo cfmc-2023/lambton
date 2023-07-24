0bd3da0808fde767334df243fea206e15dd1723c
cfmc-2023 <137046255+cfmc-2023@users.noreply.github.com>
2023-07-24 02:14:27 -0400

    Delete CMFG directory

--- CMFG/furniture-backend/api/controllers/emailController.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const send = require("../mailer/sendMial");
-
-exports.send_email = (req, res, next) => {
-  send.sendMail(req.body.voucher, req.body.email);
-};
